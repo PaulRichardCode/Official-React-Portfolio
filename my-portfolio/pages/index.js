cc51d968ef4e9a45e1804e00220549caea5b8018
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-01-07 23:10:00 +0100

    making serious progress by add tailwind utilites

--- my-portfolio/pages/index.js
+++ my-portfolio/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { BsFillMoonStarsFill } from "react-icons/bs";
 
 export default function Home() {
   return (
@@ -9,14 +10,20 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <main className=" bg-white px-10">
         <section className="min-h-screen">
-          <nav>
-            <h1>Deployedbyrichy</h1>
-            <ul>
-              <li></li>
+          <nav className=" py-10 mb-12 flex justify-between">
+            <h1 className="text-xl font-burtons">Deployedbyrichy</h1>
+            <ul className="flex items-center">
               <li>
-                <a href="#"></a>
+                <BsFillMoonStarsFill className=" cursor-pointer text-2xl" />
+              </li>
+              <li>
+                <a
+                  href="https://docs.google.com/document/d/1b0rHgz-cOFSayHc7v1c9gpqj2vRrl47h/edit?usp=sharing&ouid=101790229859116884693&rtpof=true&sd=true"
+                  className=" bg-gradient-to-r  from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8 ">
+                  Resume
+                </a>
               </li>
             </ul>
           </nav>
