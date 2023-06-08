629ae28b857a172f01a47d41af2617c343e21f32
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-06-08 13:43:06 +0100

    s

--- my-portfolio/next.config.js
+++ my-portfolio/next.config.js
@@ -1,10 +1,4 @@
 module.exports = {
   reactStrictMode: true,
   // Enable support for ECMAScript modules
-  experimental: {
-    esmExternals: true,
-  },
-  module: {
-    rules: [{ test: /\.txt$/, use: "raw-loader" }],
-  },
 };
