33dacb20208cbdb680adbc3c3bd5c5949c3b7169
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-06-08 01:42:29 +0100

    kk

--- my-portfolio/pages/contact/contact.jsx
+++ my-portfolio/pages/contact/contact.jsx
@@ -1,41 +1,15 @@
 import React from "react";
 
-import { FiGithub } from "react-icons/fi";
-import { FiTwitter } from "react-icons/fi";
-import { FiLinkedin } from "react-icons/fi";
 import { FiInstagram } from "react-icons/fi";
 import { FiMail } from "react-icons/fi";
-import { motion, useAnimation } from "framer-motion";
-import { useInView } from "react-intersection-observer";
 import { useEffect } from "react";
 
 const Contact = () => {
-  const boxVariant = {
-    visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
-    hidden: { opacity: 0, y: -100 },
-  };
-
-  const control = useAnimation();
-  const [ref, inView] = useInView();
-
-  useEffect(() => {
-    if (inView) {
-      control.start("visible");
-    } else {
-      control.start("hidden");
-    }
-  }, [control, inView]);
-
   return (
     <section
       id="contact"
       class="md:border md:border-cyan-300/5 md:w-1/2 md:mx-auto">
-      <motion.div
-        ref={ref}
-        variants={boxVariant}
-        initial="hidden"
-        animate={control}
-        class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md flex items-center justify-center flex-wrap">
+      <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md flex items-center justify-center flex-wrap">
         <h1 className="text-cyan-300 md:text-xl text-sm md:pt-10 md:mb-2 mb-2 md:mr-4 w-11/12 md:w-2/3 font-semibold tracking-tight flex items-center justify-center">
           <span className="text-cyan-300 md:text-base text-xs md:pr-4 pr-2">
             04.
@@ -45,7 +19,7 @@ const Contact = () => {
         <h1 className="text-gray-300 md:text-5xl text-3xl md:py-7 md:mb-2 mb-5 w-11/12 md:w-2/3 font-semibold tracking-tight flex items-center justify-center">
           Get In Touch
         </h1>
-        <p class="mb-8 lg:mb-16 md:w-10/12 font-normal text-center text-gray-400/80 sm:text-lg text-sm">
+        <p class="mb-8 lg:mb-16 md:w-10/12 font-normal text-center text-white sm:text-lg text-sm">
           I’m currently looking for any new opportunities, my inbox is always
           open. Whether you have a question or just want to say hi, I’ll try my
           best to get back to you!
@@ -65,23 +39,6 @@ const Contact = () => {
           {" "}
           <ul className="flex justify-around items-center">
             <li>
-              <a href="https://github.com/PaulRichardCode" target={"_blank"}>
-                <FiGithub className="text-gray-300/80 text-xl block md:hidden cursor-pointer  hover:text-cyan-300/80 hover:-translate-y-1 hover:transition duration-100" />
-              </a>
-            </li>
-            <li>
-              <a href="https://twitter.com/RichC0DE" target={"_blank"}>
-                <FiTwitter className="text-gray-300/80 text-xl block md:hidden cursor-pointer  hover:text-cyan-300/80 hover:-translate-y-1 hover:transition duration-100" />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.linkedin.com/in/richard-paul-11321124b/"
-                target={"_blank"}>
-                <FiLinkedin className="text-gray-300/80 text-xl block md:hidden cursor-pointer  hover:text-cyan-300/80 hover:-translate-y-1 hover:transition duration-100" />
-              </a>
-            </li>
-            <li>
               <a
                 href="https://www.instagram.com/richardpaul06/"
                 target={"_blank"}>
@@ -95,7 +52,7 @@ const Contact = () => {
             </li>
           </ul>
         </div>
-      </motion.div>
+      </div>
       <p className="pb-5">
         <a
           href="https://github.com/PaulRichardCode/2023-OFFICIAL-PORTFOLIO"
