d348ad026edfd7e9486a4df2c0fbcc257197f92c
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-06-08 10:08:02 +0100

    kk

--- my-portfolio/next.config.js
+++ my-portfolio/next.config.js
@@ -1,17 +1,7 @@
-/** @type {import('next').NextConfig} */
-export function webpack(config) {
-  config.module.rules.push({
-    test: /\.(mp4|webm)$/,
-    use: {
-      loader: "file-loader",
-      options: {
-        publicPath: "/_next",
-        name: "static/videos/[name].[hash].[ext]",
-      },
-    },
-  });
-
-  return config;
-}
-
-export default nextConfig;
+module.exports = {
+  reactStrictMode: true,
+  // Enable support for ECMAScript modules
+  experimental: {
+    esmExternals: true,
+  },
+};
